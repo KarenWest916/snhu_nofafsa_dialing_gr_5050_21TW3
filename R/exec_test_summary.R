7827fcb00c43b339b6fa932409f9b956d1640054
Gustafson <e.gustafson@snhu.edu>
2021-02-24 10:37:04 -0500

    eric made some adds

--- R/exec_test_summary.R
+++ R/exec_test_summary.R
@@ -10,7 +10,7 @@ source(here::here("R","create_test_summary.R"))
 create_test_summary(
   tbl_name = "nofafsa_", # name of image
 
-  input_vars = c("FAFSA Complete Rate",
+  input_vars = c("FAFSA Completion Rate",
                  "12/10/20 - 2/22/21", # Date range of test
                  "FAFSA Completion Rate - Decreased Slightly", # Test result 
                  "Additional Experimentation" ) # Recommendation
