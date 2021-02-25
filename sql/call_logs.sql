b6b0f40fa7a8fe56b1cf352e2628c7b685a3add1
Gustafson <e.gustafson@snhu.edu>
2021-02-24 19:51:29 -0500

    save changes

--- sql/call_logs.sql
+++ sql/call_logs.sql
@@ -1,8 +1,27 @@
+
+
+
 SELECT 
 UnifySalesforceContactID,
 MAX(CASE WHEN
-CallDate IS NULL THEN 0 ELSE 1 END) AS 'Dialed'
+CallDate IS NULL THEN 0 ELSE 1 END) AS [Dialed]
+--SELECT Disposition, 
+,MAX(CASE WHEN disposition IN (
+
+'DNC Request',
+'Do Not Call',
+'Follow up Call',
+'Not Using FAFSA',
+'Successful Transfer',
+'Transferred To 3rd Party'
+
+) THEN 1 ELSE 0 END) AS [Contacted]
+--Campaign, DialingList, 
+--,COUNT(1)
+--SELECT *
 FROM AARDW.[AARPL].[CallLog]
 WHERE CallDate > '2020-11-28'
-AND campaign LIKE '%fafsa%'
+AND campaign = 'No FAFSA OB Dialer' --AND Disposition = 'No Contact'
+--GROUP BY Disposition
+--Campaign, DialingList
 GROUP BY UnifySalesforceContactID
